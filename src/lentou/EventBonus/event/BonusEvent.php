db7ec157e7557e8db4105ca4caf803b7538bbe24
Lentou <33175224+Lentou@users.noreply.github.com>
2021-11-11 10:06:04 +0800

    Update BonusEvent.php

--- src/lentou/EventBonus/event/BonusEvent.php
+++ src/lentou/EventBonus/event/BonusEvent.php
@@ -11,13 +11,23 @@ use lentou\EventBonus\Main;
 
 class BonusEvent implements Listener {
 
+	private Main $plugin;
+	
+	public function __construct(Main $plugin) {
+		$this->plugin = $plugin;
+	}
+	
 	public function onJoin(PlayerJoinEvent $event) : void {
 		$player = $event->getPlayer();
-		foreach (Main::getInstance()->getConfig()->getNested("bonus") as $bonus => $value) {
+		foreach ($this->getMain()->getConfig()->getNested("bonus") as $bonus => $value) {
 			if (in_array(strtolower($player->getName()), $value["players"])) {
-				Main::getInstance()->giveEventBonus($player, $bonus);
+				$this->getMain()->giveEventBonus($player, $bonus);
 			}
 		}
+	}
+	
+	public function getMain() : Main {
+		return $this->plugin;
 	}
 	
 }
